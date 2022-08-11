58a53022dfe43a9f62c04eaccd465585a63816a9
AK <AK@example.com>
2022-08-11 22:36:30 +0200

    Ground and Air Physics

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    [SerializeField] private float _force = 10;
-    [SerializeField] private float _maxSpeed = 20;
-    [SerializeField] private float _jumpForce = 20;
+    [Header("Configure")]
+    [SerializeField] private float _force = 10f;
+    [SerializeField] private float _maxSpeed = 20f;
+    [SerializeField] private float _jumpForce = 20f;
+    [SerializeField] private float _jumpGravityScale = 0.2f;
+    [SerializeField] private float _linearDragAir = 2f;
+    [SerializeField] private float _linearDragGround = 5f;
 
     [SerializeField] private Transform _foot1Transform;
     [SerializeField] private Transform _foot2Transform;
 
     private Rigidbody2D _rb;
 
+    private float _initialGravityScale;
+
+    private float _velocityMultiplierGround = 2;
+
     private void Awake() {
         _rb = GetComponent<Rigidbody2D>();
+        _initialGravityScale = _rb.gravityScale;
     }
 
     private void Update() {
@@ -24,10 +33,16 @@ public class CharacterController : MonoBehaviour
     private void FixedUpdate() {
         _setVelocityInput();
         _setJumpInput();
+        _setLinearDrag();
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(_foot1Transform.position, _foot2Transform.position);
     }
 
     private void _setVelocityInput() {
-        _rb.AddForce(new Vector2(Input.GetAxisRaw("Horizontal") * _force, 0));
+        _rb.AddForce(new Vector2(Input.GetAxisRaw("Horizontal") * _force * (_isGrounded() ? _velocityMultiplierGround : 1), 0));
 
         if (Mathf.Abs(_rb.velocity.x) > _maxSpeed) {
             _rb.velocity = new Vector2((_rb.velocity.x < 0 ? -1 : 1) * _maxSpeed, _rb.velocity.y);
@@ -36,7 +51,23 @@ public class CharacterController : MonoBehaviour
 
     private void _setJumpInput() {
         if (Input.GetButton("Jump") && _isGrounded()) {
+            _rb.velocity = new Vector2(_rb.velocity.x, 0);
             _rb.AddForce(new Vector2(0, _jumpForce));
+        }
+
+        if (Input.GetButton("Jump") && _rb.velocity.y >= 0) {
+            _rb.gravityScale = _jumpGravityScale;
+        } else {
+            _rb.gravityScale = _initialGravityScale;
+        }
+    }
+
+    private void _setLinearDrag() {
+        if (_isGrounded()) {
+            //_rb.drag = _linearDragGround;
+            _rb.drag = _linearDragAir * _velocityMultiplierGround * (Input.GetAxisRaw("Horizontal") == 0 ? 2 : 1);
+        } else {
+            _rb.drag = _linearDragAir;
         }
     }
 
